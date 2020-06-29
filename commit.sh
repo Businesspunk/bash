b4b441c09a91a341d05d48d1f4f7edcdb407afde
Nikita <bbusinesspunk@gmail.com>
2020-06-29 15:38:29 +0300

    Updated

--- commit.sh
+++ commit.sh
@@ -1,11 +1,12 @@
 if [ -n "$1" ]
 then
-	comment=$1
+	comment="$1"
 else
 	comment=$(date '+%d.%m.%Y')
 fi
 
 git add .
-git commit -m $comment
+git commit -m "$comment"
 git push origin master
-echo -e "\e[38;5;47m Успешно отправлено"
+
+echo -e "Успешно отправлено"
